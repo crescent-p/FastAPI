ec588cd04d91887d7430b3f0f7876b9861a5c6da
Crescent <crescentp2003@gmail.com>
2025-05-11 14:41:30 +0530

    Refactor model definitions for improved readability

--- apps/models.py
+++ apps/models.py
@@ -2,17 +2,21 @@ from .database import Base
 from sqlalchemy import TIMESTAMP, Boolean, Column, ForeignKey, Integer, String, text
 from sqlalchemy.orm import relationship
 
+
 class Post(Base):
     __tablename__ = "posts"
-    
+
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, default=True)
-    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable=False)
-    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        server_default=text('now()'), nullable=False)
+    user_id = Column(Integer, ForeignKey(
+        "users.id", ondelete="CASCADE"), nullable=False)
     owner = relationship("Users")
     votes = Column(Integer, default=0, server_default='0')
+
 
 class Users(Base):
     __tablename__ = "users"
@@ -20,11 +24,13 @@ class Users(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone= True), server_default=text('now()'), nullable=False)
-
+    created_at = Column(TIMESTAMP(timezone=True),
+                        server_default=text('now()'), nullable=False)
 
 class Votes(Base):
     __tablename__ = "votes"
 
-    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False, primary_key=True)
-    posts_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), nullable=False, primary_key=True)
+    user_id = Column(Integer, ForeignKey(
+        "users.id", ondelete="CASCADE"), nullable=False, primary_key=True)
+    posts_id = Column(Integer, ForeignKey(
+        "posts.id", ondelete="CASCADE"), nullable=False, primary_key=True)
